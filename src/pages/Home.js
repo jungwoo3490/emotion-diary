01009a3c75f2d9e079e964896c91ada10a911343
jungwoo3490 <jungwoo3490@naver.com>
2023-02-17 23:41:23 +0900

    작성된 일기들을 월별로 렌더링하는 기능 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,38 @@
-import { useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import { DiaryStateContext } from '../App';
 
 import MyHeader from './../components/MyHeader';
 import MyButton from './../components/MyButton';
+import DiaryList from './../components/DiaryList';
 
 const Home = () => {
 
+    const diaryList = useContext(DiaryStateContext);
+
+    const [data, setData] = useState([]); // 해당 월에 작성된 일기들만 필터링
     const [curDate, setCurDate] = useState(new Date());
     const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+    useEffect(() => {
+        const firstDay = new Date(
+            curDate.getFullYear(),
+            curDate.getMonth(),
+            1
+        ).getTime();
+
+        const lastDay = new Date(
+            curDate.getFullYear(),
+            curDate.getMonth() + 1,
+            0
+        ).getTime();
+
+        setData(diaryList.filter((it) => it.date >= firstDay && it.date <= lastDay));
+    }, [curDate, diaryList]);
+    
+    useEffect(() => {
+        console.log(data);
+    });
+    
 
     const increaseMonth = () => {
         setCurDate(
@@ -25,6 +51,7 @@ const Home = () => {
             <MyHeader headText={headText} 
             leftChild={<MyButton text={'<'} onClick={decreaseMonth} />} 
             rightChild={<MyButton text={'>'} onClick={increaseMonth} />}/>
+            <DiaryList diaryList={data}/>
         </div>
     );
 };
