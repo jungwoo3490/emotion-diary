e9ded6b0e8d8440c28c7f44c3e28984af6428dd6
jungwoo3490 <jungwoo3490@naver.com>
2023-02-24 22:10:04 +0900

    새로운 일기 작성 페이지 컴포넌트

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,9 @@
+import DiaryEditor from "../components/DiaryEditor";
+
 const New = () => {
     return (
         <div>
-            <h1>New</h1>
-            <p>일기 작성 페이지</p>
+            <DiaryEditor />
         </div>
     );
 };
