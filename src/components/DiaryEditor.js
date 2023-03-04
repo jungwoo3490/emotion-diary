e886bf1cb83785537b411c00db04220781d81654
jungwoo3490 <jungwoo3490@naver.com>
2023-03-04 23:54:44 +0900

    감정 데이터 리스트를 별도의 파일로 분리하여 import하도록 수정

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -7,34 +7,7 @@ import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
 
 import { getStringDate } from "../util/date";
-
-const emotionList = [
-    {
-        emotion_id: 1,
-        emotion_img: process.env.PUBLIC_URL + "/assets/emotion1.png",
-        emotion_descript: "매우 좋음"
-    },
-    {
-        emotion_id: 2,
-        emotion_img: process.env.PUBLIC_URL + "/assets/emotion2.png",
-        emotion_descript: "좋음"
-    },
-    {
-        emotion_id: 3,
-        emotion_img: process.env.PUBLIC_URL + "/assets/emotion3.png",
-        emotion_descript: "보통"
-    },
-    {
-        emotion_id: 4,
-        emotion_img: process.env.PUBLIC_URL + "/assets/emotion4.png",
-        emotion_descript: "나쁨"
-    },
-    {
-        emotion_id: 5,
-        emotion_img: process.env.PUBLIC_URL + "/assets/emotion5.png",
-        emotion_descript: "매우 나쁨"
-    }
-];
+import { emotionList } from "../util/emotion";
 
 const DiaryEditor = ({ isEdit, originData }) => {
 
