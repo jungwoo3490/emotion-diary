d5dae3b8eb51ea2faa644e3b3cdc2e03af247a69
jungwoo3490 <jungwoo3490@naver.com>
2023-03-12 23:34:35 +0900

    handleClickEmote 함수 최적화 완료

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useContext, useEffect } from "react";
+import { useState, useRef, useContext, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "../App";
 
@@ -20,9 +20,9 @@ const DiaryEditor = ({ isEdit, originData }) => {
 
     const navigate = useNavigate();
 
-    const handleClickEmote = (emotion) => {
+    const handleClickEmote = useCallback((emotion) => {
         setEmotion(emotion);
-    };
+    }, []);
 
     const handleSubmit = () => {
         if (content.length < 1) {
